dba21b2c986bc661c7f93b4ce8102a912c1e2035
FangJianGuang <1312111800@qq.com>
2019-12-13 10:54:23 +0800

    添加路由

---
+++ src/router/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import router from 'vue-router';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+const routes=[];
+const router=new VueRouter({
+    router
+});
+export default router;
